077260886a0cbb3582d6a86abc95533bb00929bf
HLBoun <legendboun@gmail.com>
2025-03-31 19:01:20 -0500

    z-buff, raster, viewport, cull, colorBlend, and scissor

--- cmbst_pipeline.hpp
+++ cmbst_pipeline.hpp
@@ -9,7 +9,21 @@
 
 namespace cmbst
 {
-  struct PipelineConfigInfo {};
+  struct PipelineConfigInfo
+  {
+    VkViewport viewport;
+    VkRect2D scissor;
+    VkPipelineViewportStateCreateInfo viewportInfo;
+    VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+    VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+    VkPipelineMultisampleStateCreateInfo multisampleInfo;
+    VkPipelineColorBlendAttachmentState colorBlendAttachment;
+    VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+    VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+    VkPipelineLayout pipelineLayout = nullptr;
+    VkRenderPass renderPass = nullptr;
+    uint32_t subpass = 0;
+  };
 
   class CmbstPipeline
   {
