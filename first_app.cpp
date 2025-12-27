0a956adb76c4c8e1c74e201b67485a1ccdad54ec
HLBoun <154765503+HLBoun@users.noreply.github.com>
2025-12-26 21:06:34 -0600

    tried to add instancing triangles. did add moving triangles. 9/27
    Instancing triangles is not working because of "Semaphore still in use. May still be in use by VkSwapchainKHR" kind of error. Triangle moves but does not have copies of itself

--- first_app.cpp
+++ first_app.cpp
@@ -1,5 +1,12 @@
 #include "first_app.hpp"
 
+
+// libs
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+
 // std
 #include <stdexcept>
 #include <cassert>
@@ -8,6 +15,12 @@
 
 namespace cmbst
 {
+  struct SimplePushConstantData
+  {
+      glm::vec2 offset;
+      alignas(16) glm::vec3 color;
+  };
+
   FirstApp::FirstApp()
   {
     loadModels();
@@ -45,12 +58,18 @@ namespace cmbst
 
   void FirstApp::createPipelineLayout()
   {
+
+    VkPushConstantRange pushConstantRange{};
+    pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+    pushConstantRange.offset = 0;
+    pushConstantRange.size = sizeof(SimplePushConstantData);
+
     VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
     pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
     pipelineLayoutInfo.setLayoutCount = 0;
     pipelineLayoutInfo.pSetLayouts = nullptr;
-    pipelineLayoutInfo.pushConstantRangeCount = 0;
-    pipelineLayoutInfo.pPushConstantRanges = nullptr;
+    pipelineLayoutInfo.pushConstantRangeCount = 1;
+    pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
     if (vkCreatePipelineLayout(
           cmbstDevice.device(),
           &pipelineLayoutInfo,
@@ -137,6 +156,9 @@ namespace cmbst
 
   void FirstApp::recordCommandBuffer(int imageIndex)
   {
+    static int frame = 30;
+    frame = (frame + 1) % 100;
+
     VkCommandBufferBeginInfo beginInfo{};
 
     beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
@@ -155,7 +177,7 @@ namespace cmbst
     renderPassInfo.renderArea.extent = cmbstSwapChain->getSwapChainExtent();
 
     std::array<VkClearValue, 2> clearValues{};
-    clearValues[0].color = {0.1f, 0.1f, 0.1f, 0.1f};
+    clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
     clearValues[1].depthStencil = {1.0f, 0};
     renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
     renderPassInfo.pClearValues = clearValues.data();
@@ -175,6 +197,21 @@ namespace cmbst
 
     cmbstPipeline->bind(commandBuffers[imageIndex]);
     cmbstModel->bind(commandBuffers[imageIndex]);
+
+    for (int j = 0; j < 4; j++)
+    {
+      SimplePushConstantData push{};
+      push.offset = {-0.5f + frame * 0.02f, -0.4f + j * 0.25f};
+      push.color = {0.0f, 0.0f, 0.2f + 0.2f * j};
+
+      vkCmdPushConstants(
+          commandBuffers[imageIndex],
+          pipelineLayout,
+          VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
+          0,
+          sizeof(SimplePushConstantData),
+          &push);
+    }
     cmbstModel->draw(commandBuffers[imageIndex]);
 
     vkCmdEndRenderPass(commandBuffers[imageIndex]);
